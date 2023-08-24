83ced9c9ebfa8440aed50ef5e0ec909d60809085
Dominic Stanfford <64571534+Manta95@users.noreply.github.com>
2023-08-24 10:16:56 +0200

    Added coke_pooch + weed_pooch usable

--- server/main.lua
+++ server/main.lua
@@ -9,3 +9,25 @@ ESX.RegisterUsableItem('beer', function(source)
 	TriggerClientEvent('esx:showNotification', source, TranslateCap('used_beer'))
 
 end)
+
+---Drugs Items
+ESX.RegisterUsableItem('coke_pooch', function(source)
+       
+	local xPlayer = ESX.GetPlayerFromId(source)
+
+	xPlayer.removeInventoryItem('coke_pooch', 1)
+
+	TriggerClientEvent('esx_status:add', source, 'drug', 499000)
+	TriggerClientEvent('esx_optionalneeds:useItemCoke', source)
+	TriggerClientEvent('esx:showNotification', source, TranslateCap('used_one_coke'))
+end)
+
+ESX.RegisterUsableItem('weed', function(source)
+	local xPlayer = ESX.GetPlayerFromId(source)
+
+	xPlayer.removeInventoryItem('weed', 1)
+
+	TriggerClientEvent('esx_status:add', source, 'drug', 166000)
+	TriggerClientEvent('esx_optionalneeds:onWeed', source)
+	TriggerClientEvent('esx:showNotification', source, TranslateCap('used_one_weed'))
+end)
