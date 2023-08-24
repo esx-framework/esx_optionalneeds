7475fdccc7b366f0111245ba8c7fa6e789f1cd74
Dominic Stanfford <64571534+Manta95@users.noreply.github.com>
2023-08-24 10:25:11 +0200

    Added text for drugs

--- locales/en.lua
+++ locales/en.lua
@@ -1,5 +1,12 @@
 Locales['en'] = {
 	
 	['used_beer'] = 'you used 1x Beer',
+	['used_one_coke'] = 'you used 1x Coke Pooch',
+	['used_one_weed'] = 'you used 1x Weed',
+	
+	['overdosed'] = "you do an ~r~overdose",
+	['effects_disapear'] = "The effects are starting to wear off...",
+	['drugs_taken'] = "You feel the drug rising in your brain and taking effect...",
+	
 
 }
