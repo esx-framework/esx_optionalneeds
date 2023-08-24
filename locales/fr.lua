a96c3d5ed2854e91a7cf184fb15e2bbdb11d10a8
Dominic Stanfford <64571534+Manta95@users.noreply.github.com>
2023-08-24 10:27:15 +0200

    Added Drugs text

--- locales/fr.lua
+++ locales/fr.lua
@@ -1,5 +1,11 @@
 Locales['fr'] = {
 	
 	['used_beer'] = 'vous avez utilisé 1x Bière',
+	['used_one_coke'] = 'vous avez utilisé 1x pochon de coke',
+	['used_one_weed'] = 'vous avez utilisé 1x Weed',
+	
+	['overdosed'] = "vous avez fait une ~r~overdose",
+	['effects_disapear'] = "Les effets commencent à se dissiper...",
+	['drugs_taken'] = "Tu sens la drogue monter dans ton cerveau et faire son effet...",
 
 }
