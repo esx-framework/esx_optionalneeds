9d11aae78d9f12bb85c6d135ac33fb7909664903
Dominic Stanfford <64571534+Manta95@users.noreply.github.com>
2023-08-25 18:33:36 +0200

    Changes after review

--- client/main.lua
+++ client/main.lua
@@ -1,12 +1,10 @@
 local IsAlreadyDrunk = false
 local DrunkLevel     = -1
-local IsAlreadyDrug = false
-local DrugLevel = -1
+local IsAlreadyDrug  = false
+local DrugLevel      = -1
 
 function Drunk(level, start)
-  
   CreateThread(function()
-
     local playerPed = PlayerPedId()
 
     if start then
@@ -15,35 +13,29 @@ function Drunk(level, start)
     end
 
     if level == 0 then
-
       RequestAnimSet("move_m@drunk@slightlydrunk")
-      
+
       while not HasAnimSetLoaded("move_m@drunk@slightlydrunk") do
         Wait(0)
       end
 
       SetPedMovementClipset(playerPed, "move_m@drunk@slightlydrunk", true)
-
     elseif level == 1 then
-
       RequestAnimSet("move_m@drunk@moderatedrunk")
-      
+
       while not HasAnimSetLoaded("move_m@drunk@moderatedrunk") do
         Wait(0)
       end
 
       SetPedMovementClipset(playerPed, "move_m@drunk@moderatedrunk", true)
-
     elseif level == 2 then
-
       RequestAnimSet("move_m@drunk@verydrunk")
-      
+
       while not HasAnimSetLoaded("move_m@drunk@verydrunk") do
         Wait(0)
       end
 
       SetPedMovementClipset(playerPed, "move_m@drunk@verydrunk", true)
-
     end
 
     SetTimecycleModifier("spectator5")
@@ -53,15 +45,11 @@ function Drunk(level, start)
     if start then
       DoScreenFadeIn(800)
     end
-
   end)
-
 end
 
 function Reality()
-
   CreateThread(function()
-
     local playerPed = PlayerPedId()
 
     DoScreenFadeOut(800)
@@ -74,14 +62,11 @@ function Reality()
     SetPedMotionBlur(playerPed, false)
 
     DoScreenFadeIn(800)
-
   end)
-
 end
 
 AddEventHandler('esx_status:loaded', function(status)
-
-  TriggerEvent('esx_status:registerStatus', 'drunk', 0, '#8F15A5', 
+  TriggerEvent('esx_status:registerStatus', 'drunk', 0, '#8F15A5',
     function(status)
       if status.val > 0 then
         return true
@@ -94,16 +79,12 @@ AddEventHandler('esx_status:loaded', function(status)
     end
   )
 
-	CreateThread(function()
+  CreateThread(function()
+    while true do
+      Wait(1000)
 
-		while true do
-
-			Wait(1000)
-
-			TriggerEvent('esx_status:getStatus', 'drunk', function(status)
-				
-				if status.val > 0 then
-					
+      TriggerEvent('esx_status:getStatus', 'drunk', function(status)
+        if status.val > 0 then
           local start = true
 
           if IsAlreadyDrunk then
@@ -126,195 +107,186 @@ AddEventHandler('esx_status:loaded', function(status)
 
           IsAlreadyDrunk = true
           DrunkLevel     = level
-				end
+        end
 
-				if status.val == 0 then
-          
+        if status.val == 0 then
           if IsAlreadyDrunk then
             Reality()
           end
 
           IsAlreadyDrunk = false
           DrunkLevel     = -1
-
-				end
-
-			end)
-
-		end
-
-	end)
-
+        end
+      end)
+    end
+  end)
 end)
 
 RegisterNetEvent('esx_optionalneeds:onDrink')
 AddEventHandler('esx_optionalneeds:onDrink', function()
-  
   local playerPed = PlayerPedId()
-  
+
   TaskStartScenarioInPlace(playerPed, "WORLD_HUMAN_DRINKING", 0, 1)
   Wait(1000)
   ClearPedTasksImmediately(playerPed)
-
 end)
 
 ------DRUGS---------
 AddEventHandler('esx_status:loaded', function(status)
-	TriggerEvent('esx_status:registerStatus', 'drug', 0, '#9ec617',
-		function(status)
-			if status.val > 0 then
-				return false
-			else
-				return false
-			end
-		end, function(status)
-			status.remove(1500)
-		end)
+  TriggerEvent('esx_status:registerStatus', 'drug', 0, '#9ec617',
+    function(status)
+      if status.val > 0 then
+        return false
+      else
+        return false
+      end
+    end, function(status)
+      status.remove(1500)
+    end)
 
-	Citizen.CreateThread(function()
-		while true do
-			Wait(1000)
+  CreateThread(function()
+    while true do
+      Wait(1000)
 
-			TriggerEvent('esx_status:getStatus', 'drug', function(status)
-				if status.val > 0 then
-					local start = true
+      TriggerEvent('esx_status:getStatus', 'drug', function(status)
+        if status.val > 0 then
+          local start = true
 
-					if IsAlreadyDrug then
-						start = false
-					end
+          if IsAlreadyDrug then
+            start = false
+          end
 
-					local level = 0
+          local level = 0
 
-					if status.val <= 950000 then
-						level = 0
-					else
-						overdose()
-					end
+          if status.val <= 950000 then
+            level = 0
+          else
+            overdose()
+          end
 
-					if level ~= DrugLevel then
-					end
+          if level ~= DrugLevel then
+          end
 
-					IsAlreadyDrug = true
-					DrugLevel = level
-				end
+          IsAlreadyDrug = true
+          DrugLevel = level
+        end
 
-				if status.val == 0 then
-					if IsAlreadyDrug then
-						Normal()
-					end
+        if status.val == 0 then
+          if IsAlreadyDrug then
+            Normal()
+          end
 
-					IsAlreadyDrug = false
-					DrugLevel     = -1
-				end
-			end)
-		end
-	end)
+          IsAlreadyDrug = false
+          DrugLevel     = -1
+        end
+      end)
+    end
+  end)
 end)
 
 --When effects ends go back to normal
 function Normal()
-	Citizen.CreateThread(function()
-		local playerPed = PlayerPedId()
+  CreateThread(function()
+    local playerPed = PlayerPedId()
 
-		ClearTimecycleModifier()
-		ResetScenarioTypesEnabled()
-		ResetPedMovementClipset(playerPed, 0)
-		SetPedMotionBlur(playerPed, false)
-	end)
+    ClearTimecycleModifier()
+    ResetScenarioTypesEnabled()
+    ResetPedMovementClipset(playerPed, 0)
+    SetPedMotionBlur(playerPed, false)
+  end)
 end
 
 --In case too much drugs dies of overdose set everything back
 function overdose()
-	Citizen.CreateThread(function()
-		local playerPed = PlayerPedId()
+  CreateThread(function()
+    local playerPed = PlayerPedId()
 
-		SetEntityHealth(playerPed, 0)
-		ClearTimecycleModifier()
-		ResetScenarioTypesEnabled()
-		ResetPedMovementClipset(playerPed, 0)
-		SetPedMotionBlur(playerPed, false)
-		ESX.ShowNotification(TranslateCap('overdose'))
-	end)
+    SetEntityHealth(playerPed, 0)
+    ClearTimecycleModifier()
+    ResetScenarioTypesEnabled()
+    ResetPedMovementClipset(playerPed, 0)
+    SetPedMotionBlur(playerPed, false)
+    ESX.ShowNotification(TranslateCap('overdose'))
+  end)
 end
 
 -- Cocaine effect (Run really fast + paranoïa move + auto-jump)
 RegisterNetEvent('esx_optionalneeds:runMan')
 AddEventHandler('esx_optionalneeds:runMan', function()
-	RequestAnimSet("move_m@hurry_butch@b")
-	while not HasAnimSetLoaded("move_m@hurry_butch@b") do
-		Citizen.Wait(0)
-	end
-	onDrugs = true
-	count = 0
-	DoScreenFadeOut(1000)
-	Citizen.Wait(1000)
-	SetPedMotionBlur(PlayerPedId(), true)
-	SetTimecycleModifier("spectator5")
-	SetPedMovementClipset(PlayerPedId(), "move_m@hurry_butch@b", true)
-	SetRunSprintMultiplierForPlayer(PlayerId(), 1.2)
-	DoScreenFadeIn(1000)
-	repeat
-		ShakeGameplayCam('SMALL_EXPLOSION_SHAKE', 0.1)
-		TaskJump(PlayerPedId(), false, true, false)
-		Citizen.Wait(20000)
-		count = count + 1
-	until count == 16
-	DoScreenFadeOut(1000)
-	Citizen.Wait(1000)
-	DoScreenFadeIn(1000)
-	ClearTimecycleModifier()
-	ResetPedMovementClipset(PlayerPedId(), 0)
-	SetRunSprintMultiplierForPlayer(PlayerId(), 1.0)
-	ClearAllPedProps(PlayerPedId(), true)
-	SetPedMotionBlur(PlayerPedId(), false)
-	ESX.ShowNotification(TranslateCap('effects_disapear')
-	onDrugs = false
+  RequestAnimSet("move_m@hurry_butch@b")
+  while not HasAnimSetLoaded("move_m@hurry_butch@b") do
+    Wait(0)
+  end
+  onDrugs = true
+  count = 0
+  DoScreenFadeOut(1000)
+  Wait(1000)
+  SetPedMotionBlur(PlayerPedId(), true)
+  SetTimecycleModifier("spectator5")
+  SetPedMovementClipset(PlayerPedId(), "move_m@hurry_butch@b", true)
+  SetRunSprintMultiplierForPlayer(PlayerId(), 1.2)
+  DoScreenFadeIn(1000)
+  repeat
+    ShakeGameplayCam('SMALL_EXPLOSION_SHAKE', 0.1)
+    TaskJump(PlayerPedId(), false, true, false)
+    Wait(20000)
+    count = count + 1
+  until count == 16
+  DoScreenFadeOut(1000)
+  Wait(1000)
+  DoScreenFadeIn(1000)
+  ClearTimecycleModifier()
+  ResetPedMovementClipset(PlayerPedId(), 0)
+  SetRunSprintMultiplierForPlayer(PlayerId(), 1.0)
+  ClearAllPedProps(PlayerPedId(), true)
+  SetPedMotionBlur(PlayerPedId(), false)
+  ESX.ShowNotification(TranslateCap('effects_disapear'))
+  onDrugs = false
 end)
 
 --- Weed effect (sprint speed reduced)
 
 RegisterNetEvent('esx_optionalneeds:onWeed')
 AddEventHandler('esx_optionalneeds:onWeed', function()
-	local playerPed = PlayerPedId()
+  local playerPed = PlayerPedId()
 
-	RequestAnimSet("move_m@hipster@a")
-	while not HasAnimSetLoaded("move_m@hipster@a") do
-		Citizen.Wait(0)
-	end
+  RequestAnimSet("move_m@hipster@a")
+  while not HasAnimSetLoaded("move_m@hipster@a") do
+    Wait(0)
+  end
 
-	TaskStartScenarioInPlace(playerPed, "WORLD_HUMAN_SMOKING_POT", 0, 1)
-	Citizen.Wait(3000)
-	ClearPedTasksImmediately(playerPed)
-	SetTimecycleModifier("spectator5")
-	SetPedMotionBlur(playerPed, true)
-	SetPedMovementClipset(playerPed, "move_m@hipster@a", true)
+  TaskStartScenarioInPlace(playerPed, "WORLD_HUMAN_SMOKING_POT", 0, 1)
+  Wait(3000)
+  ClearPedTasksImmediately(playerPed)
+  SetTimecycleModifier("spectator5")
+  SetPedMotionBlur(playerPed, true)
+  SetPedMovementClipset(playerPed, "move_m@hipster@a", true)
 
-	--Effects
-	local player = PlayerId()
-	SetRunSprintMultiplierForPlayer(player, 0.7)
+  --Effects
+  local player = PlayerId()
+  SetRunSprintMultiplierForPlayer(player, 0.7)
 
-	Wait(300000)
+  Wait(300000)
 
-	SetRunSprintMultiplierForPlayer(player, 1.0)
+  SetRunSprintMultiplierForPlayer(player, 1.0)
 end)
 
 -- Useitem thread
 RegisterNetEvent('esx_optionalneeds:useItemCoke')
 AddEventHandler('esx_optionalneeds:useItemCoke', function()
+  --local lib, anim = 'anim@amb@nightclub@peds@', 'missfbi3_party_snort_coke_b_male3' -- Play animation sitting and snorting
+  local lib, anim = 'anim@mp_player_intcelebrationmale@face_palm', 'face_palm'
+  local playerPed = PlayerPedId()
+  ESX.ShowNotification(TranslateCap('drugs_taken'))
+  ESX.Streaming.RequestAnimDict(lib, function()
+    TaskPlayAnim(playerPed, lib, anim, 8.0, -8.0, -1, 32, 0, false, false, false)
 
-	--local lib, anim = 'anim@amb@nightclub@peds@', 'missfbi3_party_snort_coke_b_male3' -- Play animation sitting and snorting
-	local lib, anim = 'anim@mp_player_intcelebrationmale@face_palm', 'face_palm'
-	local playerPed = PlayerPedId()
-	ESX.ShowNotification(TranslateCap('drugs_taken')
-	ESX.Streaming.RequestAnimDict(lib, function()
-		TaskPlayAnim(playerPed, lib, anim, 8.0, -8.0, -1, 32, 0, false, false, false)
+    Wait(500)
+    while IsEntityPlayingAnim(playerPed, lib, anim, 3) do
+      Wait(0)
+      DisableAllControlActions(0)
+    end
 
-		Citizen.Wait(500)
-		while IsEntityPlayingAnim(playerPed, lib, anim, 3) do
-			Citizen.Wait(0)
-			DisableAllControlActions(0)
-		end
-
-		TriggerEvent('esx_optionalneeds:runMan')
-	end)
+    TriggerEvent('esx_optionalneeds:runMan')
+  end)
 end)
